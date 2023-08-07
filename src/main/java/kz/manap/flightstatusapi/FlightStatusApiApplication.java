2b2b08066cbc920b04a5437006ba4f242c516c70
Manas <mkadmman@gmail.com>
2023-08-07 11:46:03 +0600

    Initial commit

---
+++ src/main/java/kz/manap/flightstatusapi/FlightStatusApiApplication.java
@@ -0,0 +1,13 @@
+package kz.manap.flightstatusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightStatusApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightStatusApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/manap/flightstatusapi/FlightStatusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.manap.flightstatusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightStatusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
